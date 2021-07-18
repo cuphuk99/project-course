5a1629be3e4ac44e518abf0e16ef887fbce48d37
cuphuk99 <cuphuk99@gmail.com>
2021-07-18 18:43:13 +0200

    Initial commit

---
+++ src/main/java/edu/cuphuk/model/Item.java
@@ -0,0 +1,39 @@
+package edu.cuphuk.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Item {
+    private String id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return getId().equals(item.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/test/java/edu/cuphuk/ProjectCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cuphuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
